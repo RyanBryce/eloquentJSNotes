f7c741670939b0d91d601086e0ea4f1323e5e0ae
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-08-14 16:49:29 -0600

    notes for the introduction

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Introduction
+/*
+It is up to you to make the necessary effort. When you are struggling to follow the book, do not jump to any conclusions about your own capabilities. You are fine—you just need to keep at it.
+
+omputers themselves can do only stupidly straightforward things. The reason they are so useful is that they do these things at an incredibly high speed.
+
+Analogies that try to compare programs to objects we are familiar with tend to fall short. A superficially fitting one is that of a machine—lots of separate parts tend to be involved, and to make the whole thing tick, we have to consider the ways in which these parts interconnect and contribute to the operation of the whole.
+
+JavaScript was introduced in 1995 as a way to add programs to web pages in the Netscape Navigator browser.
+It is important to note that JavaScript has almost nothing to do with the programming language named Java.
+
+Ecma International organization that did the standardization. In practice, the terms ECMAScript and JavaScript can be used interchangeably—they are two names for the same language.
+
+*/
