16612f2fd406781c4c181f3ed8c1aa1bb72810bb
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-08-20 17:34:32 -0600

    starting chapt 3 functions

--- scripts.js
+++ scripts.js
@@ -328,13 +328,23 @@ This was done to mark this function as a constructor.
 ////Comments
 Often, raw code does not convey all the information you want a program to convey to human readers,
 or it conveys it in such a cryptic way that people might not understand it.
- At other times, you might just feel poetic or want to include some thoughts as part of your program. This is what comments are for.
+At other times, you might just feel poetic or want to include some thoughts as part of your program. This is what comments are for.
 
 
+////Summary
+You now know that a program is built out of statements, which themselves sometimes contain more statements.
+Statements tend to contain expressions, which themselves can be built out of smaller expressions.
+
+Putting statements after one another gives you a program that is executed from top to bottom.
+ You can introduce disturbances in the flow of control by using conditional (if, else, and switch) and looping (while, do, and for) statements.
+
+Variables can be used to file pieces of data under a name, and they are useful for tracking state in your program.
+ The environment is the set of variables that are defined. JavaScript systems always put a number of useful standard variables into your environment.
+
+Functions are special values that encapsulate a piece of program.
+You can invoke them by writing functionName(argument1, argument2). Such a function call is an expression, and may produce a value.
 
 */
-
-
 
 //////////////    JS Testing For Chapter 2
 
@@ -369,4 +379,11 @@ else if (num < 100)
   alert("Medium");
 else
   alert("Large");
+*/
+
+
+
+////////////////////////CHAPTER 3 Functions
+/*
+
 */
